92a6c59a9a77a902154e2a831988985fc358deb8
adrianruizmora <51535292+adrianruizmora@users.noreply.github.com>
2021-02-22 21:01:11 +0100

    module avec fonctions
    Ce module contient deux fonctions: create_json qui permet de créer un fichier json à partir d'un fichier csv sans utiliser le module panda, la deuxième fonction get_lastest_by_country permet de retrouver la valeur la plus récente des emissions totales pour le pays demandé. Encore des modifications à faire.

---
+++ fonctions.py
@@ -0,0 +1,36 @@
+import csv
+import json
+
+
+def create_json(filenameCsv):
+    filename_path_csv = filenameCsv
+    filename_path_json = 'estimates.json'
+    output = []
+
+    with open(filename_path_csv, 'r') as f:
+        next(f, None)
+        reader = csv.DictReader(f)
+        for records in reader:
+            records["id"] = records.pop("Region/Country/Area")
+            records["Region/Country/Area"] = records.pop("")
+            output.append(records)
+
+    with open(filename_path_json, 'w') as outfile:
+        json.dump(output, outfile,sort_keys=True, indent=4, ensure_ascii=False)
+    
+def get_latest_by_country(jsonFile, countryName):
+    years = []
+    emissions = str()
+    with open(jsonFile, 'r') as f:
+        info_dict = json.load(f)
+        for info in info_dict:
+            if info["Region/Country/Area"].lower() == countryName.lower():
+                years.append(info['Year'])
+                emissions = info['Value']
+    try:
+        #ATTENTION plusieurs meme annee
+        return json.dumps({"country": countryName, "year": max(years), "emissions": emissions})
+    except:
+        return None
+        
+
