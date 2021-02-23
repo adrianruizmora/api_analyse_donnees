9744a13984f336f51a7a1821b0cffb64c6a357b1
adrianruizmora <51535292+adrianruizmora@users.noreply.github.com>
2021-02-23 09:51:45 +0100

    changement des estimation de CO2
    La fonction get_latest_by_country a été modifié pour chercher les bonnes estimations de CO2

--- fonctions.py
+++ fonctions.py
@@ -24,11 +24,10 @@ def get_latest_by_country(jsonFile, countryName):
     with open(jsonFile, 'r') as f:
         info_dict = json.load(f)
         for info in info_dict:
-            if info["Region/Country/Area"].lower() == countryName.lower():
+            if info["Region/Country/Area"].lower() == countryName.lower() and 'thousand' in info['Series']:
                 years.append(info['Year'])
                 emissions = info['Value']
     try:
-        #ATTENTION plusieurs meme annee
         return json.dumps({"country": countryName, "year": max(years), "emissions": emissions})
     except:
         return None
