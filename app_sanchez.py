4241f164d22157f27c883367886b5de3b5abaa50
SanchezYoan <77174458+SanchezYoan@users.noreply.github.com>
2021-02-23 21:22:53 +0100

    Add files via upload

--- app_sanchez.py
+++ app_sanchez.py
@@ -1,6 +1,9 @@
 import csv
 import json
 
+""" function that allows to create the json file from the csv. Correction of the tables: 
+replacement of the column (RegionCountryArea) by (id), and of the column "(empty)" by (RegionCountryArea) 
+arranging of the tables so that they are ordered, and able to handle the accents."""
 
 def create_json(filename_path_csv='estimates.csv', filename_path_json='estimates.json'):
     output = list()
@@ -15,45 +18,69 @@ def create_json(filename_path_csv='estimates.csv', filename_path_json='estimates
 
     with open(filename_path_json, 'w') as outfile:
         json.dump(output, outfile,sort_keys=True, indent=4, ensure_ascii=False)
-    
+
+""" Function that allows to send back the Co2 emission of a country
+    in the last year and to send in output """
+
 def get_latest_by_country(countryName, jsonFile='estimates.json'):
     years, emissions = list(), str()
+
+    try:
+        countryName.lower()
+    except:
+        return None
 
     with open(jsonFile, 'r') as f:
         info_dict = json.load(f)
         for info in info_dict:
             if info["Region/Country/Area"].lower() == countryName.lower() and 'thousand' in info['Series']:
+                
                 years.append(info['Year'])
-                emissions = info['Value']
+                emissions = round(float(info['Value']),3)
+                emissions = str(emissions)
     try:
-        return json.dumps({"country": countryName, "year": max(years), "emissions": emissions})
+        return json.dumps({"country": countryName.lower(), "year": max(years), "emissions": emissions})
     except:
         return None
+    
+with open(filename_path_json, 'w') as outfile:
+        json.dump(output, outfile,sort_keys=True, indent=4, ensure_ascii=False)
+
+""" The function calculates the sum of the CO2 consumption of all countries 
+and divides it by the number of countries to obtain a world average. """ 
+
+def average_for_year(year):
+    emissions = []
+    with open("estimates.json", 'r') as f:
+        info_dict = json.load(f)
+        for info in info_dict:
+            if info["Year"].lower() == year.lower() and 'thousand' in info['Series']: 
+                emissions.append(float(info['Value']))
+    
+    return sum(emissions)/len(emissions)
+  
 
 # @app.route('/per_capita/<country>')
+
+""" Function that allows to return the Co2 
+emission per person of one over all years"""
+
 def per_capita(jsonFile, country):
-# Fonction qui permet de renvoyé l'émission par habitant d'un pays sur plusieurs années
+
     dic = {}
     years = []
     emissions = list()
-    # on créé une liste dict pour les émissions, et une liste pour les années
+
     with open(jsonFile, 'r') as f:
-        # on lit le fichier json
-        info_dict = json.load(f)
-        # on le charge 
+        info_dict = json.load(f)        
         for info in info_dict:
             if info["Region/Country/Area"].lower() == country.lower() and 'per capita' in info['Series']:
-                # si il trouve le pays et que per capita se trouve dans la colonne series
                 years.append(info["Year"])
-                # on ajoute l'année à la liste years
                 emissions.append(float(info["Value"]))
-                # on ajoute l'émission à la liste emissions
-
-
-
+                
     for cpt, i in enumerate(years):
         dic[i] = emissions[cpt]
-        # permet de renvoyer la valeur de l'année et l'émission pour chaque années, dans le dic
+       
 
         
     return dic
