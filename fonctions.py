eb7ce6fdafda777b6afe123f4012511caedcb2e5
DialloMamadouDjan <77362931+DialloMamadouDjan@users.noreply.github.com>
2021-02-24 14:48:05 +0100

    fonctions Adrian et Diallo
    ce fichier contient les fonctions de Adrian et de Djan ( average_for_years)

---
+++ fonctions.py
@@ -0,0 +1,64 @@
+import csv
+import json
+import os
+
+def create_json(filename_path_csv='estimates.csv', filename_path_json='estimates.json'):
+    output = list()
+         
+    if not isinstance(filename_path_csv, str) or os.path.isdir(filename_path_csv):
+        return 1
+    if not filename_path_csv.endswith('.csv') or not filename_path_json.endswith('.json'):
+        return 1
+    try:
+        f = open(filename_path_csv)
+    except FileNotFoundError:
+        return 1
+   
+    with f:
+        next(f, None)
+        reader = csv.DictReader(f)
+        for records in reader:
+            records["id"] = records.pop("Region/Country/Area")
+            records["Region/Country/Area"] = records.pop("")
+            output.append(records)
+
+    with open(filename_path_json, 'w') as outfile:
+        json.dump(output, outfile,sort_keys=True, indent=4, ensure_ascii=False)
+    
+    return 0
+
+    
+def get_latest_by_country(country_name, filename_path_json='estimates.json'):
+    years, emissions = list(), str()
+    
+    if not filename_path_json.endswith('.json'):
+        return None
+    try:
+        country_name.lower()
+    except:
+        return None
+
+    with open(filename_path_json, 'r') as f:
+        info_dict = json.load(f)
+        for info in info_dict:
+            if info["Region/Country/Area"].lower() == country_name.lower() and 'thousand' in info['Series']:
+                years.append(info['Year'])
+                emissions = round(float(info['Value']),3)
+                emissions = str(emissions)
+    try:
+        return json.dumps({"country": country_name.lower(), "year": max(years), "emissions": emissions})
+    except:
+        return None
+
+
+def average_for_year(year):
+    emissions = []
+    with open("estimates.json", 'r') as f:
+        info_dict = json.load(f)
+        for info in info_dict:
+            if info["Year"].lower() == year.lower() and 'thousand' in info['Series']:  
+                emissions.append(float(info['Value']))
+    
+    return sum(emissions)/len(emissions)
+    
+
